11aea63568ba3c5846833c21356565a3b0b98eed
pattonw <pattonw@hhmi.org>
2020-10-07 09:25:45 -0400

    add an lsd example

---
+++ example_lsd/README.md
@@ -0,0 +1,50 @@
+# DaCapo example:
+
+## 0) Setup
+It is recommended that you create a conda environment:
+
+[`conda`](https://docs.conda.io/projects/conda/en/latest/user-guide/getting-started.html)
+
+## 1) Installation
+DaCapo has some non-python dependencies. If boost is not yet installed, please install via
+
+`sudo apt install libboost-all-dev`
+
+DaCapo depends on some packages such as `waterz` that have build requirements such as `cython` and `numpy`. Install these first.
+
+`pip install -r build-requirements.txt`
+
+Then install the remaining requirements, including dacapo itself:
+
+`pip install -r requirements.txt`
+
+Finally install torch as described [here](https://pytorch.org/get-started/locally/)
+
+## 2) Get some data
+You will need:
+1) Zarr container named `data/example_data/example_data.zarr`.
+2) This container should contain 2 groups: `raw`, `validation`.
+3) Both `raw`, and `validation` should contain two arrays, `raw`, `gt`.
+
+## 2.1) Visualize your data
+If you would like to inspect your data before training on it, use
+
+`python visualize_data.py data/example_data/example_data.zarr`
+
+## 3) Data Storage
+Modify the mongodb storage in `dacapo.conf`:
+
+after `mongo_db_host` provide the url to a mongodb to which you have access
+
+after `mongo_db_name` provide a name to store your dacapo data. For example: `dacapo_v0.1`
+
+## 4) Run DaCapo
+`dacapo run --config example.conf`
+
+## 4.1) Visualize training data
+DaCapo saves snapshots during training as well as validation results. To visualize these use
+
+`python visualize_data.py runs/{run_name}/...`
+
+## 5) Visualize Results
+After DaCapo has finished running, you can visualize your results. You can use the `plot_results.ipynb` jupyter notebook (make sure you use a kernel with your virtual environment) or you can run `python plot_results.py`.
---
+++ example_lsd/neuroglancer2.py
@@ -0,0 +1,178 @@
+import daisy
+import neuroglancer
+import numpy as np
+import zarr
+import h5py
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def add_layer(context, array, name, voxel_size, array_offset, visible=True, **kwargs):
+    array_dims = len(array.shape)
+    spatial_dims = len(voxel_size)
+    assert spatial_dims == 3
+    channel_dims = array_dims - spatial_dims
+    attrs = {
+        2: {"names": ["y", "x"], "units": "nm", "scales": voxel_size},
+        3: {"names": ["z", "y", "x"], "units": "nm", "scales": voxel_size},
+        4: {
+            "names": ["c^", "z", "y", "x"],
+            "units": ["", "nm", "nm", "nm"],
+            "scales": [1, *voxel_size],
+        },
+        5: {
+            "names": ["c^", "b^", "z", "y", "x"],
+            "units": ["", "", "nm", "nm", "nm"],
+            "scales": [1, 1, *voxel_size],
+        },
+    }
+    dimensions = neuroglancer.CoordinateSpace(**attrs[array_dims])
+    offset = np.array((0,) * (channel_dims) + array_offset)
+    offset = offset // attrs[array_dims]["scales"]
+
+    channels = ",".join(
+        [
+            f"toNormalized(getDataValue({i}))" if i < array.shape[0] else "0"
+            for i in range(3)
+        ]
+    )
+    shader_4d = (
+        """
+void main() {
+  emitRGB(vec3(%s));
+}
+"""
+        % channels
+    )
+    shader_3d = """
+void main () {
+  emitGrayscale(toNormalized(getDataValue()));
+}"""
+
+    layer = neuroglancer.LocalVolume(
+        data=array, dimensions=dimensions, voxel_offset=tuple(offset)
+    )
+
+    if array.dtype == np.dtype(np.uint64):
+        context.layers.append(name=name, layer=layer, visible=visible)
+    else:
+        context.layers.append(
+            name=name,
+            layer=layer,
+            visible=visible,
+            shader=shader_4d if array_dims == 4 else shader_3d,
+            **kwargs,
+        )
+
+
+def get_volumes(h5_file, path=None):
+    datasets = []
+    try:
+        if path is None:
+            for key in h5_file.keys():
+                datasets += get_volumes(h5_file, f"{key}")
+            return datasets
+        else:
+            for key in h5_file.get(path, {}).keys():
+                datasets += get_volumes(h5_file, f"{path}/{key}")
+            return datasets
+    except AttributeError:
+        return [path]
+
+
+def add_container(
+    context,
+    snapshot_file,
+    name_prefix="",
+    volume_paths=[None],
+    graph_paths=[None],
+    graph_node_attrs=None,
+    graph_edge_attrs=None,
+    # mst=["embedding", "fg_maxima"],
+    mst=None,
+    roi=None,
+    modify=None,
+    dims=3,
+):
+    if snapshot_file.name.endswith(".zarr") or snapshot_file.name.endswith(".n5"):
+        f = zarr.open(str(snapshot_file.absolute()), "r")
+    elif snapshot_file.name.endswith(".h5") or snapshot_file.name.endswith(".hdf"):
+        f = h5py.File(str(snapshot_file.absolute()), "r")
+    with f as dataset:
+        volumes = []
+        for volume in volume_paths:
+            volumes += get_volumes(dataset, volume)
+
+        v = None
+        for volume in volumes:
+            v = daisy.open_ds(str(snapshot_file.absolute()), f"{volume}")
+            if roi is not None:
+                v = v.intersect(roi)
+            if v.dtype == np.int64:
+                v.materialize()
+                v.data = v.data.astype(np.uint64)
+            if v.dtype == np.dtype(bool):
+                v.materialize()
+                v.data = v.data.astype(np.float32)
+
+            v.materialize()
+            if modify is not None:
+                data = modify(v.data, volume)
+            else:
+                data = v.data
+
+            add_layer(
+                context,
+                data,
+                f"{name_prefix}_{volume}",
+                visible=False,
+                voxel_size=v.voxel_size[-dims:],
+                array_offset=v.roi.get_offset()[-dims:],
+            )
+
+
+def add_dacapo_snapshot(
+    context,
+    snapshot_file,
+    name_prefix="",
+    volume_paths=[None],
+    graph_paths=[None],
+    graph_node_attrs=None,
+    graph_edge_attrs=None,
+    # mst=["embedding", "fg_maxima"],
+    mst=None,
+    roi=None,
+):
+    def modify(v, name):
+        if name == "prediction":
+            v = reshape_batch_channel(v, 1, 3)
+        elif name == "raw":
+            v = reshape_batch_channel(v, 0, 2)
+        if name == "target":
+            v = reshape_batch_channel(v, 1, 3)
+        return v
+
+    add_container(
+        context,
+        snapshot_file,
+        name_prefix,
+        volume_paths,
+        graph_paths,
+        graph_node_attrs,
+        graph_edge_attrs,
+        mst,
+        roi,
+        modify,
+    )
+
+
+def reshape_batch_channel(array, batch_dim=0, concat_dim=0):
+    # Given shape (a0, a1, ..., am) and batch dim k:
+    # First remove the dim ak: new_shape = (a0, a1, ..., ak-1, ak+1, ..., am)
+    # Next replace concat_dim with -1
+
+    if batch_dim is not None:
+        array = np.concatenate(np.rollaxis(array, batch_dim), concat_dim)
+    return array
